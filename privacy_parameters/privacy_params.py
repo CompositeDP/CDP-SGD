c4ed3508bcb728c335bbdc024baaa6af20d779a4
Composite DP <132354643+CompositeDP@users.noreply.github.com>
2024-04-28 22:46:12 +0800

    Add files via upload

---
+++ privacy_parameters/privacy_params.py
@@ -0,0 +1,15 @@
+
+class Privacy_param:
+    def __init__(self, T_acc, t_ep_rate, t_ep_min, t_ep_max, T_gain, t_lr_rate, t_lr_min, t_lr_max, N):
+        self.t_ep_rate = t_ep_rate
+        self.t_ep_min = t_ep_min
+        self.t_ep_max = t_ep_max
+        self.T_acc = T_acc
+
+        self.T_gain = T_gain
+        self.t_lr_rate = t_lr_rate
+        self.t_lr_min = t_lr_min
+        self.t_lr_max = t_lr_max
+
+        self.N =N
+
